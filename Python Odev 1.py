45b6945930e6e20b4325ce36829c2bacf1bfc10b
hazardemircan <33188168+hazardemircan@users.noreply.github.com>
2020-12-22 15:00:25 +0300

    Create Python Odev 1.py

---
+++ Python Odev 1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+x=int(input("tam sayı değeri giriniz: "))
+y=float(input("ondalıklı sayı değeri giriniz: "))
+z=str(input("string değer giriniz: "))
+k=int(input("tam sayı değeri giriniz: "))
+l=str(input("string değer giriniz: "))
+
+
+# In[13]:
+
+
+print("Birinci değer: {}".format(x))
+print("İkinci değer: {}".format(x))
+print("Üçüncü değer: {}".format(z))
+print("Dördüncü değer : {}".format(k))
+print("Beşinci değer : {}".format(l))
+
+
+# In[14]:
+
+
+print(f'Birinci değer tipi: {type(x)}')
+print(f'İkinci değer tipi: {type(y)}')
+print(f'Üçüncü değer tipi: {type(z)}')
+print(f'Dördüncü değer tipi: {type(k)}')
+print(f'Beşinci değer tipi: {type(l)}')
+
+
+# In[ ]:
+
+
+
+
